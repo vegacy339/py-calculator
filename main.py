80bded455b7c6eab9c8bbd6c1140fef50ff770a9
vegacy339 <vegacy339@gmail.com>
2025-11-29 22:21:22 +0400

    Initial commit: Basic calculator with 6 operations

---
+++ main.py
@@ -0,0 +1,27 @@
+print('PyCalculator')
+
+while True:
+    number_one = int(input('Enter number 1: '))
+    number_two = int(input('Enter number 2: '))
+    print('+ - addition\n- - subtraction\n/ - division\n* - multiplication\n** - exponentiation\n// - Division without remainder')
+    operator = input('Select operator (Or quit to exit): ')
+
+    if operator == '+':
+        print(f'Calculation result: {number_one + number_two}')
+    elif operator == '-':
+        print(f'Calculation result: {number_one - number_two}')
+    elif operator == '/':
+        if number_two != 0:
+            print(f'Calculation result: {number_one / number_two}')
+        else:
+            print(f'Error: Division by zero!')
+    elif operator == '*':
+        print(f'Calculation result: {number_one * number_two}')
+    elif operator == '**':
+        print(f'Calculation result: {number_one ** number_two}')
+    elif operator == '//':
+        print(f'Calculation result: {number_one // number_two}')
+    elif operator == 'quit':
+        break
+    else:
+        print('Incorrect operator!')
